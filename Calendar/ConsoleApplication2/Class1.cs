bc54cb703d21984666d1cac1d862254bb55c5382
Christoffer Pedersen <stofferpedersen@gmail.com>
2012-09-25 15:51:40 +0200

    added persistence class

---
+++ Calendar/ConsoleApplication2/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calendar
+{
+    /// <summary>
+    /// Persistence layer of the calendar application
+    /// </summary>
+    class Persistence
+    {
+    }
+}
