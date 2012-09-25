9bc29299038cd993743a2ff9c1b910b6755d47ea
Christoffer Pedersen <stofferpedersen@gmail.com>
2012-09-25 15:48:05 +0200

    Made event methods and changed namespace

--- Calendar/ConsoleApplication2/Event.cs
+++ Calendar/ConsoleApplication2/Event.cs
@@ -3,9 +3,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Event
+using Domain;
+
+namespace EventSpace
 {
+    /// <summary>
+    /// Class representing an event in the calendar system
+    /// </summary>
     class Event
     {
+        /// <summary>
+        /// Unique ID for this event
+        /// </summary>
+        public int id { get; set; }
+        
+        /// <summary>
+        /// Title of this event 
+        /// </summary>
+        public string title { get; set; }
+        
+        /// <summary>
+        /// Description for this event
+        /// </summary>
+        public string description { get; set; }
+
+        /// <summary>
+        /// Location of the event
+        /// </summary>
+        public latLng location;
+
+        /// <summary>
+        /// Start time of the event
+        /// </summary>
+        public DateTime start { get; set; }
+
+        /// <summary>
+        /// End time of the event
+        /// </summary>
+        public DateTime end { get; set; }
+
+        /// <summary>
+        /// All messages posted to this event
+        /// </summary>
+        public string[] messages;
+
+        /// <summary>
+        /// A list of users who are invited to the event
+        /// </summary>
+        public User[] userList;
+
+        /// <summary>
+        /// Immutable struct representing a latitude longitude
+        /// </summary>
+        public struct latLng
+        {
+            private readonly double latitude;
+            private readonly double longitude;
+
+            public double lat { get { return latitude; } }
+            public double lng { get { return longitude; } }
+
+            public latLng(double latitude, double longitude)
+            {
+                this.latitude = latitude;
+                this.longitude = longitude;
+            }
+
+            public string toString()
+            {
+                return string.Format("{0},{1}", latitude, longitude);
+            }
+        }
+
+        /// <summary>
+        /// Method used for sending invitations to the registered users
+        /// It should only send to those who have not yet received an invitation
+        /// </summary>
+        public void sendInvitations()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
