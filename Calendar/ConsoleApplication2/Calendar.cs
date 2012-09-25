fd0bd24eb56ed7e33d6a234dee2f1ddea2e790af
Ellen Engdahl <ellen.engdahl@gmail.com>
2012-09-25 15:12:47 +0200

    stubs for Calendar

--- Calendar/ConsoleApplication2/Calendar.cs
+++ Calendar/ConsoleApplication2/Calendar.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using EventSpace;
 
 namespace Domain
 {
     class Calendar
     {
+        private User ID { get;set; }  
+
+        private HashSet<EventSpace> events;
+
+
+
+
     }
 }
