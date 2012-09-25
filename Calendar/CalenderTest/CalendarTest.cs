d216b9204be37d30d5c22b0b0399eccc9f9b6f19
Ellen Engdahl <ellen.engdahl@gmail.com>
2012-09-25 16:11:16 +0200

    added CalendarTest

---
+++ Calendar/CalenderTest/CalendarTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalenderTest
+{
+    [TestClass]
+    public class CalendarTest
+    {
+        [TestMethod]
+        public void TestCreateEvent()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestDeleteEvent()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestGetEvent()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestRespond()
+        {
+
+        }
+    }
+}
